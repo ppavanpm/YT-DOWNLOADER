036a3d41c21d2a1dbd87db81d880628200c76a61
ppavanpm <indiaspsbsb@gmail.com>
2025-05-30 13:45:28 +0530

    Create VideoCard.jsx

---
+++ frontend/src/components/VideoCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VideoCard = ({ video }) => {
+  return (
+    <div className="video-card">
+      <h3>{video.title}</h3>
+      {/* Add more video details and styling */}
+    </div>
+  );
+};
+
+export default VideoCard;
